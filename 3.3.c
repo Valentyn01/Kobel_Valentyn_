78fddb7f71661f731bc5df6a04fa01c6b72db502
Valentyn01 <65087151+Valentyn01@users.noreply.github.com>
2020-09-23 19:24:03 +0300

    Add files via upload

---
+++ 3.3.c
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int t,z,y,a,b;
+    printf("z=");
+    scanf("%d",&z);
+    printf("y=");
+    scanf("%d",&y);
+    b=z-2*y;
+    if(b!=0){
+        a=3*(y/b);
+        if(a>0){
+            t=sqrt(a)-y*z;
+            printf("%d",t); 
+        }
+        else
+        {
+            printf("Помилка"); 
+        }
+    }
+    else
+    {
+       printf("Помилка"); 
+    }
+    return 0;
+}
